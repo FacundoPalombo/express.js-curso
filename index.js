0b699d056423a17aa97572fae99de1c93086c1eb
Facundo Palombo <facundo.palombo@gmail.com>
2019-03-04 13:38:53 -0300

    Finaliza la implementación de las plantillas del sitio y los assets

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
-const app = require('express')()
+const express = require('express')
+const app = express();
+const path = require('path')
+const productsRouter = require('./routes/products')
+
+
 const port = process.env.PORT || 4200;
+
+app.use('/static', express.static(path.join(__dirname,'public')))
 
 app.get('/', (req,res) => {
     res.send('Hello world!')
 })
+
+app.use('/products', productsRouter)
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
+
+
 
 const server = app.listen(port, ()=> {
     console.log(`Server on port ${port}`)
