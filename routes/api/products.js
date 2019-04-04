3cf8a189d1d05c7b365be33e9ca4bc153c62357c
Facundo Palombo <facundo.palombo@gmail.com>
2019-04-04 09:57:01 -0300

    Se manejan las validaciones con Joi y se crean los schemas

--- routes/api/products.js
+++ routes/api/products.js
@@ -1,13 +1,21 @@
 const router = require('express').Router();
 const ProductsService = require('../../services/products')
+const {
+    productIdSchema,
+    productTagSchema,
+    createProductSchema,
+    updateProductSchema
+} = require('../../utils/schemas/products')
+const validation = require('../../utils/middlewares/validationHandler')
 
 const productsService = new ProductsService();
 
 router.get('/', async (req, res, next) => {
-    const { tags } = req.query;
+    const {
+        tags
+    } = req.query;
     console.log('req: ', req.query)
     try {
-        throw new Error('this is an error')
         const product = await productsService.getProducts({
             tags
         });
@@ -21,10 +29,14 @@ router.get('/', async (req, res, next) => {
 })
 
 router.get('/:productId', async (req, res, next) => {
-    const { productId } = req.params;
+    const {
+        productId
+    } = req.params;
     console.log('req: ', req.params)
     try {
-        const product = await productsService.getProduct({ productId })
+        const product = await productsService.getProduct({
+            productId
+        })
         res.status(200).json({
             data: product,
             message: 'products retrieved'
@@ -34,11 +46,15 @@ router.get('/:productId', async (req, res, next) => {
     }
 })
 
-router.post('/', async (req, res, next) => {
-    const { body: product } = req;
+router.post('/', validation(createProductSchema), async (req, res, next) => {
+    const {
+        body: product
+    } = req;
     console.log('req: ', req)
     try {
-        const createdProduct = productsService.createProduct({ product })
+        const createdProduct = productsService.createProduct({
+            product
+        })
         res.status(201).json({
             data: createdProduct,
             message: 'product created'
@@ -48,27 +64,45 @@ router.post('/', async (req, res, next) => {
     }
 })
 
-router.put('/:productId', async (req, res, next) => {
-    const { productId } = req.params;
-    const { body: product } = req;
-    console.log('req: ', req.params)
-    try {
-        const updatedProduct = productsService.updateProduct({ productId , product })
-        res.status(200).json({
-            data: updatedProduct,
-            message: 'product updated'
-        })
-    } catch (error) {
-        next(error)
-    }
-})
+router.put('/:productId',
+    validation({
+        productId: productIdSchema
+    }, "params"),
+    validation(updateProductSchema, "params"), async (req, res, next) => {
+        const {
+            productId
+        } = req.params;
+        const {
+            body: product
+        } = req;
+        console.log('req: ', req.params)
+        try {
+            const updatedProduct = productsService.updateProduct({
+                productId,
+                product
+            })
+            res.status(200).json({
+                data: updatedProduct,
+                message: 'product updated'
+            })
+        } catch (error) {
+            next(error)
+        }
+    })
 
 router.patch('/:productId', async (req, res, next) => {
-    const { productId } = req.params;
-    const { body: product } = req;
+    const {
+        productId
+    } = req.params;
+    const {
+        body: product
+    } = req;
     console.log('req: ', req.params)
     try {
-        const patchedProduct = productsService.patchProduct({ productId , product })
+        const patchedProduct = productsService.patchProduct({
+            productId,
+            product
+        })
         res.status(200).json({
             data: patchedProduct,
             message: 'product patched'
@@ -79,10 +113,14 @@ router.patch('/:productId', async (req, res, next) => {
 })
 
 router.delete('/:productId', async (req, res, next) => {
-    const { productId } = req.params;
+    const {
+        productId
+    } = req.params;
     console.log('req: ', req.params)
     try {
-        const deletedProduct = productsService.deleteProduct({ productId })
+        const deletedProduct = productsService.deleteProduct({
+            productId
+        })
         res.status(200).json({
             data: deletedProduct,
             message: 'product deleted'
